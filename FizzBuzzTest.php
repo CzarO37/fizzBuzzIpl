5ecf1206770eb2a485c0b49d99d58c2dabb8638d
Cezary Mnich <cezary.mnich@student.vinci.be>
2020-10-20 17:00:46 +0200

    add - count for input 2

--- FizzBuzzTest.php
+++ FizzBuzzTest.php
@@ -19,4 +19,12 @@ class FizzBuzzTest extends TestCase
         //Assert
         $this->assertEquals(1,$actual);
     }
+
+    public function test_count_given2_shouldReturn2() {
+        $fizzBuzz = new FizzBuzz();
+
+        $actual = $fizzBuzz->count(2);
+
+        $this->assertEquals(2,$actual);
+    }
 }
