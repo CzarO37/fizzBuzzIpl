b970b9c5788dbffad55895c0f36c367c7cd26341
Cezary Mnich <cezary.mnich@student.vinci.be>
2020-10-20 17:23:40 +0200

    refator - return the string value of the int

--- FizzBuzz.php
+++ FizzBuzz.php
@@ -7,9 +7,9 @@ class FizzBuzz
 
     public function count(int $int): string
     {
-        if ($int === 2) return "2";
-        else if($int === 3) return "Fizz";
 
-        return "1";
+        if($int === 3) return "Fizz";
+
+        return (string) $int;
     }
 }
